fba1de1afae4df1a575bef5c661c46c348d30f7c
Builtbyjb <awotideajibola@gmail.com>
2025-05-27 09:45:52 -0400

    setup tide

--- setup.py
+++ setup.py
@@ -3,22 +3,27 @@ if __name__ == "__main__":
   import subprocess
 
   os_name = platform.system()
-  # Check is python3 and node is installed
-
   # Install npm packages
   result = subprocess.run(["npm", "install"], capture_output=True, text=True)
   print(result)
-
-  # install tide
   if os_name == "Darwin" or os_name == "Linux":
-  # Install uv
+    # Install uv
     result = subprocess.run(["pip3", "install", "uv"], capture_output=True, text=True)
     print(result)
+    # Install python packages
+    result = subprocess.run(["uv", "sync"], capture_output=True, text=True)
+    print(result)
+    # install tide
+    result = subprocess.run(
+      ["curl", "-L" "-o", "tide", "https://github.com/Builtbyjb/tide/releases/tag/v0.1.0"],
+      capture_output=True,
+      text=True
+      )
+    print(result)
+    # TODO: Add tide to path
   elif os_name == "Windows":
     result = subprocess.run(["pip", "install", "uv"], capture_output=True, text=True)
     print(result)
-  else:
-    print(" Unable to determine operating system.")
+  else: print("Unable to determine operating system.")
 
-  print(os_name)
-  print("Setting up complete")
+  print("Setting up complete.")
