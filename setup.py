dbda5d08b67ec987bbdf3448fbdfb3b7e9f6d346
Builtbyjb <awotideajibola@gmail.com>
2025-05-27 10:09:37 -0400

    bugfix

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
   # Install npm packages
   result = subprocess.run(["npm", "install"], capture_output=True, text=True)
   print(result)
+  # Install uv
+  result = subprocess.run(["pip", "install", "uv"], capture_output=True, text=True)
+  print(result)
+  # Install python packages
+  result = subprocess.run(["uv", "sync"], capture_output=True, text=True)
+  print(result)
   if os_name == "Darwin" or os_name == "Linux":
-    # Install uv
-    result = subprocess.run(["pip3", "install", "uv"], capture_output=True, text=True)
-    print(result)
-    # Install python packages
-    result = subprocess.run(["uv", "sync"], capture_output=True, text=True)
-    print(result)
     # install tide
     result = subprocess.run(
       ["curl", "-L" "-o", "tide", "https://github.com/Builtbyjb/tide/releases/tag/v0.1.0"],
