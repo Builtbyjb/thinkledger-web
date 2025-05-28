cccbe243c08b2111e097c8c2458890c4a1bf8118
Builtbyjb <awotideajibola@gmail.com>
2025-05-28 14:31:44 -0400

    setup progress bar

--- setup.py
+++ setup.py
@@ -1,37 +1,73 @@
+def progress_bar(progress, total) -> None:
+  percent = 100 * (progress / float(total))
+  bar = "#" * int(percent) + "-" * (100 - int(percent))
+  print(f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
 if __name__ == "__main__":
   import platform
   import subprocess
   import sys
 
+  total = 6
   os_name = platform.system()
-  print("Operating System: ", os_name)
+  print("Setting up thinkledger for operating system: ", os_name)
+
   try:
+    progress_bar(1, total)
+
     # Install npm packages
-    subprocess.run(["npm", "install"])
+    subprocess.run(
+      ["npm", "install"],
+      stdout=subprocess.DEVNULL,
+      stderr=subprocess.STDOUT,)
+    progress_bar(2, total)
+
     if os_name == "Darwin" or os_name == "Linux":
       # Install uv
       subprocess.run(
         "curl -LsSf https://astral.sh/uv/install.sh | sh",
         shell=True,
-        executable="/bin/bash"
-        )
-      subprocess.run("uv sync", shell=True, executable="/bin/bash", capture_output=True)
-      # install tide
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+        executable="/bin/bash")
+      progress_bar(3, total)
+
+      # Install dependencies
       subprocess.run(
-        "curl -L -o tide https://github.com/Builtbyjb/tide/releases/tag/v0.1.0",
-        shell=True,
-        executable="/bin/bash"
-        )
-      # TODO: Add tide to path
-      # run a install script
+         "uv sync",
+         shell=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+        executable="/bin/bash")
+      progress_bar(4, total)
+
+      # Install tide
+      # subprocess.run(
+      #   "curl -LsSf https://raw.githubusercontent.com/builtbyjb/tide/main/install.sh | sh",
+      #   shell=True,
+      #   executable="/bin/bash")
+      progress_bar(5, total)
+
     elif os_name == "Windows":
-      subprocess.run(["pip", "install", "uv"], capture_output=True, text=True)
+      # Install uv
+      progress_bar(3, total)
+      # Run uv sync
+      progress_bar(4, total)
+      # Install tide
+      progress_bar(5, total)
+
     else: print("Unable to determine operating system.")
+
   except FileNotFoundError as f_err:
     print(f_err)
     sys.exit(1)
+
   except Exception as e:
     print(e)
     sys.exit(1)
 
-  print("Setting up complete.")
+  progress_bar(6, total)
+
+  # Progress_bar
+  print("\nSetup complete. Run 'tide run dev --watch' to get started")
