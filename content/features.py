66bf021340657596424c7e28ad9604b95993b55f
Mona Lisa <anthonyrichardo080@gmail.com>
2025-06-10 12:43:28 +0100

    make some changes

--- content/features.py
+++ content/features.py
@@ -18,7 +18,7 @@ FEATURES = [
   },
   {
     "title": "AI Powered Financial Analysis",
-    "icon":  "fa-solid fa-chart-line text-2xl",
+    "icon":  """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column-icon lucide-chart-no-axes-column"><line x1="18" x2="18" y1="20" y2="10"/><line x1="12" x2="12" y1="20" y2="4"/><line x1="6" x2="6" y1="20" y2="14"/></svg>""",
     "description": """We use Artificial Intelligence (AI) to analyze your financial
     data, helping you identify issues and quickly access the information needed
     to resolve them."""
